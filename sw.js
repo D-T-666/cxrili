5e96464f0426d2e8a7f15bf3838ce51c8864c254
Dimitri Tabatadze <tabatadzedima20@gmail.com>
2021-03-27 16:59:34 +0400

    Updates

--- sw.js
+++ sw.js
@@ -12,23 +12,3 @@ self.addEventListener("activate", function (e) {
 			clients.forEach((client) => client.navigate(client.url));
 		});
 });
-
-self.addEventListener("fetch", (evt) => {
-	if (evt.request.url === "https://d-t-666.github.io/cxrili/") {
-		evt.respondWith(
-			new Response(
-				new Blob(
-					[
-						"<html><body><h1>გამთიშე და თავიდან ჩამრთე გასაახლებლად</h1><h3>სხვანაირად ვერ იხილავ ვებსაიტს</h3></body></html>",
-					],
-					{
-						type: "text/html",
-					}
-				),
-				{ status: 200 }
-			)
-		);
-	} else {
-		evt.respondWith(fetch(evt.request.url));
-	}
-});
