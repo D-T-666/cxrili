49234f883d23494fee682cd400b7771a9e575c6e
D-T-666 <tabatadzedima20@gmail.com>
2021-04-29 14:32:59 +0400

    depricating this url

--- sw.js
+++ sw.js
@@ -1,12 +1,14 @@
-self.addEventListener("install", () => {
+self.addEventListener("install", function (e) {
 	self.skipWaiting();
 });
 
-self.addEventListener("activate", () => {
+self.addEventListener("activate", function (e) {
 	self.registration
 		.unregister()
-		.then(self.clients.matchAll())
-		.then((clients) =>
-			clients.forEach((client) => client.navigate(client.url))
-		);
+		.then(function () {
+			return self.clients.matchAll();
+		})
+		.then(function (clients) {
+			clients.forEach((client) => client.navigate("https://cxrili.netlify.app/"));
+		});
 });
