9efb72cf84deb1f97603449d7ff32924247a709e
D-T-666 <tabatadzedima20@gmail.com>
2021-05-26 12:00:58 +0400

    Homeworks page boilerplate in place

---
+++ src/components/homeworkView/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style.scss';
+
+const HomeworkView = () => {
+	return (
+		<div className="homework-page content-box">
+			homework
+		</div>
+	)
+};
+
+export default HomeworkView;
