b1dcf7afb0b123089456abe36ba2936ed9e94cf7
D-T-666 <tabatadzedima20@gmail.com>
2021-06-08 10:17:31 +0400

    minor functionall and visual improvements to scroll selector

--- src/components/assets/ScrollSelector/index.js
+++ src/components/assets/ScrollSelector/index.js
@@ -1,19 +1,41 @@
-import React, { useState } from "react"
+import React, { useRef, useState } from "react"
 import "./style.scss"
 
-const ScrollSelector = ({ options, onSelect, className, style }) => {
-  const [active, setActive] = useState(options[0])
+const ScrollSelector = ({
+  options,
+  onSelect,
+  className,
+  placeholder = undefined
+}) => {
+  const activeRef = useRef() // Reference to the current active option
+  const [active, setActive] = useState(placeholder) // Current active option
 
   return (
     <ul className={"scroll-selector" + (className ? " " + className : "")}>
       {options.map((option) => (
         <li
-          style={style ? style : {}}
-          className={option + (option == active ? " active" : "")}
+          key={option}
+          ref={option === active ? activeRef : undefined} // Asign ref to this object if it's active
+          className={option + (option == active ? " active" : "")} // Asign the "active" class if it's active
           onClick={(e) => {
             e.stopPropagation()
+
             if (active !== option) {
+              // scrooll this element into view after 200 miliseconds
+              setTimeout(
+                () =>
+                  activeRef.current.scrollIntoView({
+                    inline: "center",
+                    block: "nearest",
+                    behavior: "smooth"
+                  }),
+                200,
+                false
+              )
+
               setActive(option)
+
+              // Call the event listener
               onSelect(option, e)
             }
           }}>
