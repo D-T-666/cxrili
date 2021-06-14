ecfd877e34446d31821576a22b45142f01a1099d
D-T-666 <tabatadzedima20@gmail.com>
2021-06-14 13:30:05 +0400

    switched week-day-bar to use ScrollSelector

--- src/components/dayView/weekDayBar/WeekDayBar.jsx
+++ src/components/dayView/weekDayBar/WeekDayBar.jsx
@@ -1,25 +1,25 @@
-import React from 'react';
-import DayButton from "./DayButton";
-import 'css/dayView/week-day-bar/week-day-bar.scss';
+import React from "react"
+import DayButton from "./DayButton"
+import "css/dayView/week-day-bar/week-day-bar.scss"
+import ScrollSelector from "components/assets/ScrollSelector"
 
 const WeekDayBar = ({ onDayChange, day }) => {
-	const dayNames = ["ორშაბათი", "სამშაბათი", "ოთხშაბათი", "ხუთშაბათი", "პარასკევი"];
+  const dayNames = [
+    "ორშაბათი",
+    "სამშაბათი",
+    "ოთხშაბათი",
+    "ხუთშაბათი",
+    "პარასკევი"
+  ]
 
-	return (
-		<ul className="week-day-bar">
-			{dayNames.map((dayName, idx) => 
-				<DayButton 
-					key={dayName} 
-					onClick={onDayChange} 
-					name={idx} 
-					active={idx === day}
-					className={(idx === day? "active": "")}
-				>
-					{dayName}
-				</DayButton>
-			)}
-		</ul>
-	);
+  return (
+    <ScrollSelector
+      options={dayNames}
+      onSelect={onDayChange}
+      placeholder={dayNames[day]}
+      className='week-day-bar'
+    />
+  )
 }
 
-export default WeekDayBar;
+export default WeekDayBar
