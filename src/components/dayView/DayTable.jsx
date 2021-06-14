4cc41950bbf2c17ee1e763d761bc7ded0b2337f2
D-T-666 <tabatadzedima20@gmail.com>
2021-06-14 13:30:32 +0400

    formatting

--- src/components/dayView/DayTable.jsx
+++ src/components/dayView/DayTable.jsx
@@ -1,146 +1,156 @@
-import React, { useState, useEffect } from 'react';
-import { useNotes } from 'contexts/NotesContext';
-import { useAuth } from 'contexts/AuthContext';
-import ClassBlock from "components/dayView/classBlock/ClassBlock";
-import TotalTimer from 'components/dayView/classBlock/timers/TotalTimer';
+import React, { useState, useEffect } from "react"
+import { useNotes } from "contexts/NotesContext"
+import { useAuth } from "contexts/AuthContext"
+import ClassBlock from "components/dayView/classBlock/ClassBlock"
+import TotalTimer from "components/dayView/classBlock/timers/TotalTimer"
 
 const TimeTable = (props) => {
-	const [ tables, setTables ] = useState([]);
-	const [ timeRange, setTimeRange ] = useState([]);
-	const [ notesSet, setNotesSet ] = useState(0);
-	const [ tablesReady, setTablesReady ] = useState(false);
+  const [tables, setTables] = useState([])
+  const [timeRange, setTimeRange] = useState([])
+  const [notesSet, setNotesSet] = useState(0)
+  const [tablesReady, setTablesReady] = useState(false)
 
-	const [ blockExpanded, setBlockExpanded ] = useState(null);
+  const [blockExpanded, setBlockExpanded] = useState(null)
 
-	const { notes } = useNotes();
-	const { users, currentUser } = useAuth();
+  const { notes } = useNotes()
+  const { users, currentUser } = useAuth()
 
-	const processTables = (data) => {
-		return data.tables.map((table, dayIndex) => {
-			let blocks = table.classes;
-			
-			const len = blocks.length-1;
-			for(let i = 0; i < len; i++)
-				if (blocks[i].end !== blocks[i+1].start)
-					blocks.push({
-						name: "break",
-						start: blocks[i].end,
-						end: blocks[i+1].start
-					});
+  const processTables = (data) => {
+    return data.tables.map((table, dayIndex) => {
+      let blocks = table.classes
 
-			blocks = blocks.map((block, index) => {
-				const start = block.start.split(":");
-				const end = block.end.split(":");
-				return {
-					...block,
-					teacher: data.teachers[block.name],
-					int_start: parseInt(start[0])*60 + parseInt(start[1]),
-					int_end: parseInt(end[0])*60 + parseInt(end[1]),
-					id: block.name+"s"+block.start+"e"+block.end+"d"+table.name,
-					upVoters: [],
-					downVoters: [],
-					notes: [],
-					classIndex: index,
-					dayIndex: dayIndex
-				};
-			});
+      const len = blocks.length - 1
+      for (let i = 0; i < len; i++)
+        if (blocks[i].end !== blocks[i + 1].start)
+          blocks.push({
+            name: "break",
+            start: blocks[i].end,
+            end: blocks[i + 1].start
+          })
 
-			// Return blocks sorted by start time 
-			return blocks.sort((a, b) => a.int_start - b.int_start);
-		});
-	};
+      blocks = blocks.map((block, index) => {
+        const start = block.start.split(":")
+        const end = block.end.split(":")
+        return {
+          ...block,
+          teacher: data.teachers[block.name],
+          int_start: parseInt(start[0]) * 60 + parseInt(start[1]),
+          int_end: parseInt(end[0]) * 60 + parseInt(end[1]),
+          id:
+            block.name + "s" + block.start + "e" + block.end + "d" + table.name,
+          upVoters: [],
+          downVoters: [],
+          notes: [],
+          classIndex: index,
+          dayIndex: dayIndex
+        }
+      })
 
-	useEffect(() => {
-		fetch(`/timetable/11g.json`)
-		.then(res => res.json())
-		.then(data => {
-			const tables = processTables(data);
-			const today = tables[props.day];
-			const int_start = today[0].int_start;
-			const int_end = today[today.length-1].int_end;
+      // Return blocks sorted by start time
+      return blocks.sort((a, b) => a.int_start - b.int_start)
+    })
+  }
 
+  useEffect(() => {
+    fetch(`/timetable/11g.json`)
+      .then((res) => res.json())
+      .then((data) => {
+        const tables = processTables(data)
+        const today = tables[props.day]
+        const int_start = today[0].int_start
+        const int_end = today[today.length - 1].int_end
 
-			setTables(tables)
-			setTimeRange([int_start, int_end])
-			setTablesReady(true)
-		})
-	}, [])
+        setTables(tables)
+        setTimeRange([int_start, int_end])
+        setTablesReady(true)
+      })
+  }, [])
 
-	useEffect(() => {
-		// Assign votes
-		if(currentUser && notesSet) {
-			console.log("yeo")
-			setTables(
-				tables.map(table => 
-					table.map(block => {
-						block.notes.forEach(note => {
-							let currentVote = 0;
+  useEffect(() => {
+    // Assign votes
+    if (currentUser && notesSet) {
+      console.log("yeo")
+      setTables(
+        tables.map((table) =>
+          table.map((block) => {
+            block.notes.forEach((note) => {
+              let currentVote = 0
 
-							if(note.upVoters.includes(currentUser.uid)) currentVote = 1
-							if(note.downVoters.includes(currentUser.uid)) currentVote = 2;
+              if (note.upVoters.includes(currentUser.uid)) currentVote = 1
+              if (note.downVoters.includes(currentUser.uid)) currentVote = 2
 
-							note.currentVote = currentVote;
-						})
+              note.currentVote = currentVote
+            })
 
-						return {...block};
-					})
-				)
-			)
-		}
-	}, [currentUser, notesSet]);
+            return { ...block }
+          })
+        )
+      )
+    }
+  }, [currentUser, notesSet])
 
-	useEffect(() => {
-		// Set up notes
-		if(tablesReady && notes && users) {
-			const timeNow = Date.now()
-			setTables(
-				tables.map((table, dayIndex) => {
-					return table.map((block) => {
-						const blockNotes = [];
-						notes.forEach(note => {
-							if(note.day === dayIndex && note.class === block.name)
-								blockNotes.push({
-									...note,
-									currentVote: 0,
-									createdAt: note.createdAt !== null ? note.createdAt.seconds * 1000 : timeNow,
-									authorName: users[note.author] ? users[note.author].name : "unknown user",
-									authorPhotoURL: users[note.author] ? users[note.author].photoURL : ""
-								});
-						})
+  useEffect(() => {
+    // Set up notes
+    if (tablesReady && notes && users) {
+      const timeNow = Date.now()
+      setTables(
+        tables.map((table, dayIndex) => {
+          return table.map((block) => {
+            const blockNotes = []
+            notes.forEach((note) => {
+              if (note.day === dayIndex && note.class === block.name)
+                blockNotes.push({
+                  ...note,
+                  currentVote: 0,
+                  createdAt:
+                    note.createdAt !== null
+                      ? note.createdAt.seconds * 1000
+                      : timeNow,
+                  authorName: users[note.author]
+                    ? users[note.author].name
+                    : "unknown user",
+                  authorPhotoURL: users[note.author]
+                    ? users[note.author].photoURL
+                    : ""
+                })
+            })
 
-						return {...block, notes: blockNotes};
-					})
-				})
-			)
-			setNotesSet(old => old+1);
-		};
-	}, [notes, users, tablesReady])
-	
-	return (
-		<div className="content-box">
-			<ul className="time-table-timeline">
-				{
-					tables.length > 0 && tables[props.day].map((block, blockIndex) => 
-						<ClassBlock
-							key={block.id} 
-							blockIndex={blockIndex}
-							classData={block}
-							isToday={props.today === props.day}
-							blockExpanded={blockExpanded}
-							setBlockExpanded={setBlockExpanded}/>
-					)
-				}
-				{
-					props.today === props.day && timeRange[0] && timeRange[1] &&	
-					<TotalTimer
-						int_start={timeRange[0]}
-						int_end={timeRange[1]}
-						shouldUpdate={props.today === props.day}
-					/>
-				}
-			</ul>
-		</div>
-	);
-};
+            return { ...block, notes: blockNotes }
+          })
+        })
+      )
+      setNotesSet((old) => old + 1)
+    }
+  }, [notes, users, tablesReady])
 
-export default TimeTable;
+  useEffect(() => {
+    console.log(props.day)
+  }, [])
+
+  return (
+    <div className='content-box'>
+      <ul className='time-table-timeline'>
+        {tables.length > 0 &&
+          tables[props.day].map((block, blockIndex) => (
+            <ClassBlock
+              key={block.id}
+              blockIndex={blockIndex}
+              classData={block}
+              isToday={props.today === props.day}
+              blockExpanded={blockExpanded}
+              setBlockExpanded={setBlockExpanded}
+            />
+          ))}
+        {props.today === props.day && timeRange[0] && timeRange[1] && (
+          <TotalTimer
+            int_start={timeRange[0]}
+            int_end={timeRange[1]}
+            shouldUpdate={props.today === props.day}
+          />
+        )}
+      </ul>
+    </div>
+  )
+}
+
+export default TimeTable
--- src/components/dayView/DayViewPage.jsx
+++ src/components/dayView/DayViewPage.jsx
@@ -1,46 +1,44 @@
-import React, { Component } from 'react';
-import WeekDayBar from "components/dayView/weekDayBar/WeekDayBar";
-import DayTable from "components/dayView/DayTable";
-import { BrowserRouter as Router } from 'react-router-dom';
-import { NotesProvider } from 'contexts/NotesContext';
+import React, { Component } from "react"
+import WeekDayBar from "components/dayView/weekDayBar/WeekDayBar"
+import DayTable from "components/dayView/DayTable"
+import { BrowserRouter as Router } from "react-router-dom"
+import { NotesProvider } from "contexts/NotesContext"
 
 class DayViewPage extends Component {
-	constructor(props) {
-		super(props);
+  constructor(props) {
+    super(props)
 
-		const date = new Date();
-		const day = date.getDay();
-		const today = day < 1 || day > 5 ? false : day - 1;
-		this.state = {
-			colorTheme: "light-theme",
-			day: parseInt(props.match.params.d || String(today || 0)),
-			today: today,
-		};
+    const date = new Date()
+    const day = date.getDay()
+    const today = day < 1 || day > 5 ? false : day - 1
+    this.state = {
+      colorTheme: "light-theme",
+      day: parseInt(props.match.params.d || String(today || 0)),
+      today: today
+    }
 
-		this.changeDay = this.changeDay.bind(this);
-	}
+    this.changeDay = this.changeDay.bind(this)
+  }
 
-	changeDay(day) {
-		// Set state.day to the argument provided
-		this.setState({day});
-	}
+  changeDay(_, day, e) {
+    // Set state.day to the argument provided
+    this.setState({ day })
+    window.history.pushState("", "", `/day/${day}`)
+  }
 
-	render() {
-		return (
-			<Router basename="/day">
-				<WeekDayBar
-					onDayChange={this.changeDay}
-					day={parseInt(this.state.day)}
-				/>
-				<NotesProvider>
-					<DayTable
-						today={this.state.today}
-						day={this.state.day}
-					/>
-				</NotesProvider>
-			</Router>
-		);
-	}
+  render() {
+    return (
+      <Router basename='/day'>
+        <WeekDayBar
+          onDayChange={this.changeDay}
+          day={parseInt(this.state.day)}
+        />
+        <NotesProvider>
+          <DayTable today={this.state.today} day={this.state.day} />
+        </NotesProvider>
+      </Router>
+    )
+  }
 }
 
-export default DayViewPage;
+export default DayViewPage
